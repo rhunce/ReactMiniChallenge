c6bf3e09ef6fd5324fbaaec6ce53cfb7eabfeafb
Danielle Blom <dlblom91@gmail.com>
2020-11-23 10:58:06 -0600

    Update tests and add verbose to test script

---
+++ tests/components.test.js
@@ -0,0 +1,64 @@
+import React from "react";
+import App from "../src/App.jsx";
+import FactsList from "../src/FactsList.jsx";
+import Fact from "../src/Fact.jsx";
+import animalFacts from "../src/data.jsx";
+import { shallow, mount } from "enzyme";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  let appWrapper = shallow(<App />);
+
+  it("renders a facts list", () => {
+    const factsList = appWrapper.find(FactsList);
+
+    expect(factsList).toHaveLength(1);
+  });
+
+  it("is a class component which holds state: 'facts'", () => {
+    const appState = appWrapper.state();
+
+    expect(appState.facts).toBeDefined();
+  });
+
+  it("passes facts to factsList through props", () => {
+    const factsList = appWrapper.find(FactsList);
+
+    expect(factsList.props().facts).toEqual(appWrapper.state().facts);
+  });
+});
+
+describe("FactsList", () => {
+  let factsListWrapper = shallow(<FactsList facts={animalFacts} />);
+
+  it("renders 12 animal facts", () => {
+    const fact = factsListWrapper.find(Fact);
+
+    expect(fact).toHaveLength(12);
+  });
+});
+
+describe("Fact", () => {
+  it("recieves access to a animal, fact and image through props", () => {
+    const props = {
+      id: 1,
+      animal: "Sea Otter",
+      fact:
+        "Sea Otters hold hands while they're sleeping so they don't drift apart.",
+      image: "https://jooinn.com/images/otter-11.jpg",
+    };
+
+    let FactComponenet = mount(<Fact {...props} />);
+
+    expect(FactComponenet.prop("animal")).toBe("Sea Otter");
+    expect(FactComponenet.prop("image")).toBe(
+      "https://jooinn.com/images/otter-11.jpg"
+    );
+    expect(FactComponenet.prop("fact")).toBe(
+      "Sea Otters hold hands while they're sleeping so they don't drift apart."
+    );
+  });
+});
