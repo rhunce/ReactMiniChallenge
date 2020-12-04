913c3638831310e263d5b1349f1beeb01e104b23
Danielle Blom <dlblom91@gmail.com>
2020-12-04 15:11:16 -0600

    Update tests

--- tests/components.test.js
+++ tests/components.test.js
@@ -12,22 +12,34 @@ configure({ adapter: new Adapter() });
 describe("App", () => {
   let appWrapper = shallow(<App />);
 
-  it("renders a facts list", () => {
+  it("is a class component which holds state: 'facts' & 'favorite'", () => {
+    const appState = appWrapper.state();
+
+    expect(appState.facts).toBe(animalFacts);
+    expect(appState.favorite).toBeDefined();
+  });
+
+  it("renders the FactsList component", () => {
     const factsList = appWrapper.find(FactsList);
 
     expect(factsList).toHaveLength(1);
   });
 
-  it("is a class component which holds state: 'facts'", () => {
-    const appState = appWrapper.state();
-
-    expect(appState.facts).toBeDefined();
-  });
-
-  it("passes facts to factsList through props", () => {
+  it("passes facts and to factsList through props", () => {
     const factsList = appWrapper.find(FactsList);
 
     expect(factsList.props().facts).toEqual(appWrapper.state().facts);
+  });
+
+  beforeEach(() => {
+    const event = {
+      target: { value: "Corgi" },
+    };
+    appWrapper.instance().handleFavoriteClick(event);
+  });
+
+  it("updates the favorite state when a favorite animal is selected", () => {
+    expect(appWrapper.state().favorite).toEqual("Corgi");
   });
 });
 
@@ -38,5 +50,19 @@ describe("FactsList", () => {
     const fact = factsListWrapper.find(Fact);
 
     expect(fact).toHaveLength(12);
+  });
+});
+
+describe("Fact", () => {
+  const mockFavorite = jest.fn();
+  const props = { fact: animalFacts[0], handleFavoriteClick: mockFavorite };
+  const fact = shallow(<Fact {...props} />);
+
+  beforeEach(() => {
+    fact.find("button").simulate("click");
+  });
+
+  it("calls the handleFavoriteClick function when the 'Click to favorite' button is clicked", () => {
+    expect(mockFavorite).toHaveBeenCalled();
   });
 });
